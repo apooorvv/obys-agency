6b0b4ed3de0a60fd12dbb16932e4e1cfdfd9920a
apooorvv <apoorvnamdeo2004@gmail.com>
2025-03-18 21:30:19 +0530

    Page1 done

--- script.js
+++ script.js
@@ -1,48 +1,73 @@
 
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 
-tl.from(".line h1",{
-    y:+150,
-    stagger:0.2,
-    duration:0.6,
-    delay:0.5
-})
+    tl.from(".line h1",{
+        y:+150,
+        stagger:0.2,
+        duration:0.6,
+        delay:0.5
+    })
 
-tl.from("#line1-part1",{
-    opacity:0,
-    onStart: function(){
-        var h5timer = document.querySelector('#line1-part1 h5')
-        var grow = 0
-        setInterval(function(){
-            if(grow<100){
-                h5timer.innerHTML = grow++
-            }
-            else{
-                h5timer.innerHTML = grow
-            }
-        },30)
-    },
-})
+    tl.from("#line1-part1",{
+        opacity:0,
+        onStart: function(){
+            var h5timer = document.querySelector('#line1-part1 h5')
+            var grow = 0
+            setInterval(function(){
+                if(grow<100){
+                    h5timer.innerHTML = grow++
+                }
+                else{
+                    h5timer.innerHTML = grow
+                }
+            },30)
+        },
+    })
 
-tl.to(".line h2",{
-    animationName:"anime",
-    opacity:1
-})
+    tl.to(".line h2",{
+        animationName:"anime",
+        opacity:1
+    })
 
-tl.to("#loader",{
-    opacity:0,
-    duration:0.2,
-    delay:3
-})
+    tl.to("#loader",{
+        opacity:0,
+        duration:0.2,
+        delay:3
+    })
 
-tl.from("#page1",{
-    delay:0.2,
-    y:1600,
-    opacity:0,
-    duration:0.5,
-    ease:Power4
-})
+    tl.from("#page1",{
+        delay:0.2,
+        y:1600,
+        duration:0.5,
+        ease:Power4
+    })
 
-tl.to("#loader",{
-    display:"none"
-})
+    tl.to("#loader",{
+        display:"none"
+    })
+
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1",{
+        y:120,
+        stagger:0.1
+    })
+
+    tl.from("#nav",{
+        opacity:0
+    })
+}
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#cursor",{
+            left:dets.x,
+            top:dets.y,
+    
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+
+loadingAnimation()
+cursorAnimation()
