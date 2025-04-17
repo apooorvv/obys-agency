5586fa5aa58c67972d31fcdf7182784e4775ebee
apooorvv <apoorvnamdeo2004@gmail.com>
2025-04-18 02:53:02 +0530

    Few Changes

--- script.js
+++ script.js
@@ -101,8 +101,6 @@ function cursorAnimation(){
 
     Shery.makeMagnet("#nav-part2 h4");
     
-
-    
 }
 
 function SheryJSAnimation(){
@@ -184,9 +182,49 @@ function videoContainerEffect(){
     })
 }
 
+// function footerAnimation() {
+
+//     var clutter = ""
+//     var clutter2 = ""
+//     document.querySelector("#footer h1").textContent.split("").forEach(function (elem) {
+//       clutter += `<span>${elem}</span>`
+//     })
+//     document.querySelector("#footer h1").innerHTML = clutter
+//     document.querySelector("#footer h2").textContent.split("").forEach(function (elem) {
+//       clutter2 += `<span>${elem}</span>`
+//     })
+//     document.querySelector("#footer h2").innerHTML = clutter2
+  
+  
+//     document.querySelector("#footer-text").addEventListener("mouseenter", function () {
+//       gsap.to("#footer h1 span", {
+//         opacity: 0,
+//         stagger: 0.05
+//       })
+//       gsap.to("#footer h2 span", {
+//         delay: 0.35,
+//         opacity: 1,
+//         stagger: 0.1
+//       })
+//     })
+//     document.querySelector("#footer-text").addEventListener("mouseleave", function () {
+//       gsap.to("#footer h1 span", {
+//         opacity: 1,
+//         stagger: 0.1,
+//         delay: 0.35,
+  
+//       })
+//       gsap.to("#footer h2 span", {
+//         opacity: 0,
+//         stagger: 0.05
+//       })
+//     })
+// }
+
 loadingAnimation()
 cursorAnimation()
 locomotiveAnimation()
 SheryJSAnimation()
 FlagAnimation()
 videoContainerEffect()
+// footerAnimation()
